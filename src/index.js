d83a56e71a62c785cc222ddb6cfbea902637f258
judithukachukwu <ukachukwujudith95@gmail.com>
2025-07-05 20:16:07 +0100

    use one language

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
@@ -8,9 +8,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 /* GLOBAL VARIABLES */
 
 window.$primaryLanguage = 'en';
-window.$secondaryLanguage = 'pl';
+// window.$secondaryLanguage = 'pl';
 window.$primaryLanguageIconId = 'primary-lang-icon';
-window.$secondaryLanguageIconId = 'secondary-lang-icon';
+// window.$secondaryLanguageIconId = 'secondary-lang-icon';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.register();
