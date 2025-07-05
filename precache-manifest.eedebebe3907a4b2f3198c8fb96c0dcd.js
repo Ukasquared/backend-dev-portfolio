e50f5ea87a44c5c1fa4d549a1607134c1c2b92fe
judithukachukwu <ukachukwujudith95@gmail.com>
2025-07-05 04:09:40 +0100

    Updates

---
+++ precache-manifest.eedebebe3907a4b2f3198c8fb96c0dcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e77a8e2db8d987527f9605e6ea8c4438",
+    "url": "/backend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "39f7c3949aec84e93cab",
+    "url": "/backend-dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "edbb62b6d65d8e3ff61e",
+    "url": "/backend-dev-portfolio/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "39f7c3949aec84e93cab",
+    "url": "/backend-dev-portfolio/static/js/2.a3c320d7.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/backend-dev-portfolio/static/js/2.a3c320d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edbb62b6d65d8e3ff61e",
+    "url": "/backend-dev-portfolio/static/js/main.3a708447.chunk.js"
+  },
+  {
+    "revision": "c63a9d40b0055e10668b",
+    "url": "/backend-dev-portfolio/static/js/runtime-main.659408c0.js"
+  }
+]);
